416d111e627699ed36e94664ceac560f4aefcc85
Giedrius Statkevičius <giedrius.statkevicius@vinted.com>
2022-09-15 15:06:03 +0300

    prometheus_client: implement example request

--- src/prometheus_client.rs
+++ src/prometheus_client.rs
@@ -1,4 +1,17 @@
-use hyper::Client;
+use hyper::body::HttpBody as _;
+use hyper::{Body, Client, Request};
+use prometheus_copy::{label_matcher, read_request, LabelMatcher, Query, ReadRequest};
+use prost::Message;
+use snap::raw::Encoder;
+use tokio::io::{stdout, AsyncWriteExt as _};
+
+mod thanos {
+    include!("thanos.rs");
+}
+
+mod prometheus_copy {
+    include!("prometheus_copy.rs");
+}
 
 #[derive(Default)]
 pub struct PrometheusClient {
@@ -12,11 +25,37 @@ impl PrometheusClient {
         // Still inside `async fn main`...
         let client = Client::new();
 
+        let readRequest = ReadRequest {
+            accepted_response_types: vec![read_request::ResponseType::StreamedXorChunks as i32],
+            queries: vec![Query {
+                start_timestamp_ms: 0,
+                end_timestamp_ms: 2663243063000,
+                hints: None,
+                matchers: vec![LabelMatcher {
+                    name: "__name__".to_string(),
+                    value: "go_gc_duration_seconds".to_string(),
+                    r#type: label_matcher::Type::Eq as i32,
+                }],
+            }],
+        };
+
+        let encodedReq = readRequest.encode_to_vec();
+        let mut encoder = Encoder::new();
+        let compressedReq = encoder.compress_vec(&encodedReq).unwrap();
+
         // Parse an `http::Uri`...
-        let uri = self.url.parse().unwrap();
+        let req = Request::builder()
+            .method("POST")
+            .uri(self.url.clone() + "/api/v1/read")
+            .body(Body::from(compressedReq))
+            .unwrap();
 
         // Await the response...
-        let mut resp = client.get(uri).await.unwrap();
+        let mut resp = client.request(req).await.unwrap();
+
+        while let Some(chunk) = resp.body_mut().data().await {
+            stdout().write_all(&chunk.unwrap()).await.unwrap();
+        }
 
         println!("Response: {}", resp.status());
     }
